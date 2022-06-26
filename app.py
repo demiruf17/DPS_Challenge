da67e1b69d2a7e2bb1007f5f7d700f460feb8b45
demiruf17 <ufuk_demir_95@hotmail.com>
2022-06-26 05:05:44 +0200

    Web service is implemented

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from train import load
+import numpy as np
+
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+app.config["JSONIFY_MIMETYPE"] = "application/json; charset=utf-8"
+
+model = load("models/random_forest.pickle")
+
+@app.route('/result',methods = ['POST'])
+def result():
+    # parse request
+    jo = request.get_json()
+    year = jo['year']
+    month = jo['month']
+
+    input = np.zeros((9,4))
+    
+
+    for i in range(3):
+        for j in range(3):
+            input[i*3 + j,:] = np.array([i, j, year, month])
+        
+
+    # make prediction
+    value = model.predict(input)
+    value = value.sum()
+
+    # return response
+    response = jsonify({"prediction":value })
+    response.status_code = 200
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    return response
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
